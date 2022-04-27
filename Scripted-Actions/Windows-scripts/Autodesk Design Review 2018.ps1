d5d7e4eb3422e3db92b401a24b0748e59da1ef63
ManagedServicesIT <104450769+ManagedServicesIT@users.noreply.github.com>
2022-04-27 11:10:56 -0400

    Create Autodesk Design Review 2018.ps1

---
+++ Scripted-Actions/Windows-scripts/Autodesk Design Review 2018.ps1
@@ -0,0 +1,12 @@
+#description: Installs Autodesk Design Review 2018 via Chocolatey Package Manager (https://chocolatey.org/)
+#execution mode: Combined
+#tags: Nerdio, Apps install, Chocolatey
+<#
+This script installs Autodesk Design Review 2018 via Chocolatey
+#>
+
+# Install Chocolatey if it isn't already installed
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+# Install Adobe Reader DC
+choco install designreview
