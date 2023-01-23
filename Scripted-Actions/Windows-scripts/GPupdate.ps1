68677e6cf45b340bbf7ab4b8fb29ace8ce991ec3
ManagedServicesIT <104450769+ManagedServicesIT@users.noreply.github.com>
2023-01-23 16:52:14 -0500

    Update GPupdate.ps1

--- Scripted-Actions/Windows-scripts/GPupdate.ps1
+++ Scripted-Actions/Windows-scripts/GPupdate.ps1
@@ -1,4 +1,11 @@
-cd\
-c:
-gpupdate /force
-exit
+:Start
+cls
+@echo off
+
+Set /P Computer=Enter the Computer Name:
+
+psexec \\RAPP.patriothomecare.onmicrosoft.com gpupdate /force
+
+Goto End
+
+:End
