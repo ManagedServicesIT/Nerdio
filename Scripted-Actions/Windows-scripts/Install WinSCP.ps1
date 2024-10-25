3a42787ecc096b9f5cb7248b0a2296a5bbd5ac4e
ManagedServicesIT <104450769+ManagedServicesIT@users.noreply.github.com>
2024-10-25 08:56:29 -0400

    Update Install WinSCP.ps1

--- Scripted-Actions/Windows-scripts/Install WinSCP.ps1
+++ Scripted-Actions/Windows-scripts/Install WinSCP.ps1
@@ -8,5 +8,5 @@ This script installs WinSCP via Chocolatey
 # Install Chocolatey if it isn't already installed
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
-# Install Adobe Reader DC
+# Install WinSCP
 choco install winscp.install -y
