f1e86c89195e215beb229c82ba02176c7f32d28a
ManagedServicesIT <104450769+ManagedServicesIT@users.noreply.github.com>
2024-10-25 08:40:27 -0400

    Update Installs AutoCAD LT 2022.241510 via Chocolatey.ps1

--- Scripted-Actions/Windows-scripts/Installs AutoCAD LT 2022.241510 via Chocolatey.ps1
+++ Scripted-Actions/Windows-scripts/Installs AutoCAD LT 2022.241510 via Chocolatey.ps1
@@ -1,12 +1,12 @@
-#description: Installs AutoCAD LT 2022.241510 via Chocolatey Package Manager (https://chocolatey.org/)
+#description: Installs AutoCAD LT via Chocolatey Package Manager (https://chocolatey.org/)
 #execution mode: Combined
 #tags: Nerdio, Apps install, Chocolatey
 <#
-This script installs AutoCAD LT 2022.241510 via Chocolatey
+This script installs AutoCAD LT via Chocolatey
 #>
 
 # Install Chocolatey if it isn't already installed
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
-# Install Adobe Reader DC
+# Install AutoCAD LT
 choco install autocadlt /DesktopIcon -y
